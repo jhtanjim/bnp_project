bc8decfaa5b0c02d4186459743907340c3d9618d
jhtanjim <jawadhossaintanjim@gmail.com>
2025-01-21 17:53:53 +0600

    done voting

--- app/approveList/approveList.jsx
+++ app/approveList/approveList.jsx
@@ -120,9 +120,9 @@ const ApproveList = () => {
                             ? "Approving..."
                             : "অনুমোদন"}
                         </button>
-                        <button className="px-4 py-1 text-sm bg-red-500 hover:bg-red-600 text-white rounded">
+                        {/* <button className="px-4 py-1 text-sm bg-red-500 hover:bg-red-600 text-white rounded">
                           বাতিল
-                        </button>
+                        </button> */}
                       </div>
                     </td>
                   </tr>
